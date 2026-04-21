7cdef57b0d74cd4c543d8fd09e2080a6e9bfe579
Tomek Walkuski <git@tomekw.com>
2026-04-21 10:27:50 +0200

    Error handling

--- src/twins-main.adb
+++ src/twins-main.adb
@@ -17,9 +17,7 @@ begin
       use Tackle;
 
       Arguments : constant Opts.Argument_List := Opts.Consume_Arguments;
-
       Result : constant Opts.Result := Opts.Parse (Arguments, Configs.Options);
-      Cfg : constant Configs.Config := Configs.Parse (Result);
    begin
       if Result.Has_Flag ("help") then
          Opts.Print_Usage ("twins", Configs.Options);
@@ -29,26 +27,37 @@ begin
       end if;
 
       declare
-         Server_Acceptor : Acceptors.Acceptor;
-
-         Workers_Pool : array (1 .. Cfg.Workers_Count) of Workers.Worker;
+         Cfg : constant Configs.Config := Configs.Parse (Result);
       begin
-         for Worker of Workers_Pool loop
-            Worker.Init (Cfg);
-         end loop;
+         declare
+            Server_Acceptor : Acceptors.Acceptor;
+            Workers_Pool : array (1 .. Cfg.Workers_Count) of Workers.Worker;
+         begin
+            for Worker of Workers_Pool loop
+               Worker.Init (Cfg);
+            end loop;
 
-         Server_Acceptor.Init (Cfg);
+            Server_Acceptor.Init (Cfg);
 
-         Shutdown_Handlers.Shutdown_Handler.Wait;
-         Acceptors.Shutdown;
-         Workers.Shutdown (Cfg.Workers_Count);
+            Shutdown_Handlers.Shutdown_Handler.Wait;
+            Acceptors.Shutdown;
+            Workers.Shutdown (Cfg.Workers_Count);
+         end;
+      exception
+         when E : others =>
+            Acceptors.Shutdown;
+            Workers.Shutdown (Cfg.Workers_Count);
+            Loggers.Shutdown;
+
+            Text_IO.Put_Line (Text_IO.Standard_Error, "twins: " & Exceptions.Exception_Message (E));
+            Text_IO.New_Line (Text_IO.Standard_Error);
+            Text_IO.Put_Line (Text_IO.Standard_Error, "Run 'twins --help' for usage.");
+            Command_Line.Set_Exit_Status (Command_Line.Failure);
       end;
 
       Loggers.Shutdown;
    exception
       when E : others =>
-         Acceptors.Shutdown;
-         Workers.Shutdown (Cfg.Workers_Count);
          Loggers.Shutdown;
 
          Text_IO.Put_Line (Text_IO.Standard_Error, "twins: " & Exceptions.Exception_Message (E));
